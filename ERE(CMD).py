764dd2621b785f86130ab7c4b440e20523222ee3
Busjr <Busjr1337@mail.ru>
2024-07-09 10:54:01 +0300

    addition

--- ERE(CMD).py
+++ ERE(CMD).py
@@ -1,34 +1,70 @@
 import pyAesCrypt
 
-print("███████╗██████╗░███████╗")
-print("██╔════╝██╔══██╗██╔════╝")
-print("█████╗░░██████╔╝█████╗░░")
-print("██╔══╝░░██╔══██╗██╔══╝░░")
-print("███████╗██║░░██║███████╗")
-print("╚══════╝╚═╝░░╚═╝╚══════╝ v.0.3 by busjr")
+print("""
+███████╗██████╗░███████╗
+██╔════╝██╔══██╗██╔════╝
+█████╗░░██████╔╝█████╗░░
+██╔══╝░░██╔══██╗██╔══╝░░
+███████╗██║░░██║███████╗
+███████╗██║░░██║███████╗
+"╚══════╝╚═╝░░╚═╝╚══════╝ v.0.4 by busjr""")
 
-print('[1] - Encrypt')
-print('[2] - Decipher')
-
+def Choice():
+    print("""----------------------------------------
+[1] - Encrypt
+[2] - Decipher
+[3] - Back
+[4] - Exit""")
+    input_user = int(input("Choice:"))
+    if input_user == 1:
+        Encrypt()
+    elif input_user == 2:
+        Decrypt()
+    elif input_user == 4:
+        exit()
+    else:
+        Choice()
 
 def Encrypt_Ere(dir): 
-    password = input("password: ")
-    pyAesCrypt.encryptFile(dir, dir + ".aes", password)
+    try:
+        input_user = input("password: ")
+        if input_user != '3':
+            password = input_user
+            pyAesCrypt.encryptFile(dir, dir + ".aes", password)
+            Choice()
+        else:
+            Choice()
+    except ValueError:
+            print('File not found or file extension not supported.(Please check the full path to the file or its .txt extension.)')
 
 def Decipher_Ere(dir):
-    password = input("password: ")
-    pyAesCrypt.decryptFile(dir, dir + " dataout.txt", password)
+    try:
+        input_user = input("password: ")
+        if input_user != '3':
+            password = input_user
+            pyAesCrypt.decryptFile(dir, dir + " dataout.txt", password)
+            Choice()
+        else:
+            Choice()
+    except ValueError:
+        print('File not found or file extension not supported.(Please check the full path to the file or its .aes extension.)')
 
 def Encrypt():
-    dir = input('Path file for encrypt: ')
-    dir.replace("/", "\\")
-    Encrypt_Ere(dir)
+    input_user = input('Path file for encrypt: ')
+    if input_user != '3':
+        dir = input_user
+        dir.replace("/", "\\")
+        Encrypt_Ere(dir)
+    else:
+        Choice()
+    
+def Decrypt():
+    input_user = input('Path file for decrypt: ')
+    if input_user != '3':
+        dir = input_user
+        dir.replace("/", "\\")
+        Decipher_Ere(dir)
+    else:
+        Choice()
 
-a = input("Choice:")
-a = int(a)
-if a == 1:
-    Encrypt()
-elif a == 2:
-    dir = input('Path file for decrypt: ')
-    dir.replace("/", "\\")
-    Decipher_Ere(dir)
+Choice()
