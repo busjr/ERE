07ad8009a46fb0882bfdebd03cd1176e296d64dc
Busjr <Busjr1337@mail.ru>
2024-07-11 15:35:13 +0300

    fix: menu, func

--- ERE(CMD).py
+++ ERE(CMD).py
@@ -1,69 +1,64 @@
 import pyAesCrypt
-
-print("""
+from os import system
+from getpass import getpass
+    
+def Choice():
+    try:
+        print("""
 ███████╗██████╗ ███████╗
 ██╔════╝██╔══██╗██╔════╝
 █████╗  ██████╔╝█████╗  
 ██╔══╝  ██╔══██╗██╔══╝  
 ███████╗██║  ██║███████╗
-╚══════╝╚═╝  ╚═╝╚══════╝v.0.4 by busjr""")
-
-def Choice():
-    print("""----------------------------------------
+╚══════╝╚═╝  ╚═╝╚══════╝v.0.4 by busjr
+--------------------------------------
 [1] - Encrypt
 [2] - Decipher
 [3] - Back
 [4] - Exit""")
-    input_user = int(input("Choice:"))
-    if input_user == 1:
-        Encrypt()
-    elif input_user == 2:
-        Decrypt()
-    elif input_user == 4:
-        exit()
-    else:
-        Choice()
-
-def Encrypt_Ere(dir): 
-    try:
-        input_user = input("password: ")
-        if input_user != '3':
-            password = input_user
-            pyAesCrypt.encryptFile(dir, dir + ".aes", password)
-            Choice()
+        input_user = int(input(">"))
+        if input_user == 1:
+            Encrypt()
+        elif input_user == 2:
+            Decrypt()
+        elif input_user == 4:
+            exit()
         else:
+            system('cls')
             Choice()
     except ValueError:
-            print('File not found or file extension not supported.(Please check the full path to the file or its .txt extension and open the program with administrator rights.)')
+        system('cls')
+        Choice()
 
-def Decipher_Ere(dir):
+def Encrypt(): 
     try:
-        input_user = input("password: ")
-        if input_user != '3':
-            password = input_user
-            pyAesCrypt.decryptFile(dir, dir + " dataout.txt", password)
-            Choice()
+        path = input('Path file for encrypt: ')
+        if  path != '3':
+            path.replace("/", "\\")
+            password = getpass("password: ",  stream="*")
+            pyAesCrypt.encryptFile(path, path + ".aes", password)
         else:
+            system('cls')
             Choice()
     except ValueError:
-        print('File not found or file extension not supported.(Please check the full path to the file or its .aes extension and open the program with administrator rights.)')
+            system('cls')
+            print("\033[31m" + 'ERORRE: File not found or file extension not supported.(Please check the full path to the file or its .aes extension and open the program with administrator rights.' + "\033[0m")
+            Choice()
 
-def Encrypt():
-    input_user = input('Path file for encrypt: ')
-    if input_user != '3':
-        dir = input_user
-        dir.replace("/", "\\")
-        Encrypt_Ere(dir)
-    else:
-        Choice()
-    
 def Decrypt():
-    input_user = input('Path file for decrypt: ')
-    if input_user != '3':
-        dir = input_user
-        dir.replace("/", "\\")
-        Decipher_Ere(dir)
-    else:
+    try:
+        path = input('Path file for decrypt: ')
+        if path != '3':
+            path.replace("/", "\\")
+            password = getpass("password: ", stream="*")
+            pyAesCrypt.decryptFile(path, path + " dataout.txt", password)
+        else:
+            system('cls')
+            Choice()
+    except ValueError:
+        system('cls')
+        print("\033[31m" + 'ERORRE: File not found or file extension not supported.(Please check the full path to the file or its .aes extension and open the program with administrator rights.' + "\033[0m")
         Choice()
 
-Choice()
+if __name__ == "__main__":       
+    Choice()
