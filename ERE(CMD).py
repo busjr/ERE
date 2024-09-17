4265ef63fd9f0e6e29d83bd6434fe238019077e5
busjr <busjr1337@mail.ru>
2024-09-17 22:32:50 +0300

    add: Context menu

--- ERE(CMD).py
+++ ERE(CMD).py
@@ -1,3 +1,5 @@
+import sys
+import subprocess
 from os import system
 from getpass import getpass
 from pyAesCrypt import encryptFile, decryptFile
@@ -5,68 +7,155 @@ from pyAesCrypt import encryptFile, decryptFile
 
 def Choice():
     try:
-        print("""
+        print(
+            """
 ███████╗██████╗ ███████╗
 ██╔════╝██╔══██╗██╔════╝
 █████╗  ██████╔╝█████╗
 ██╔══╝  ██╔══██╗██╔══╝
 ███████╗██║  ██║███████╗
-╚══════╝╚═╝  ╚═╝╚══════╝v.0.4 by busjr
+╚══════╝╚═╝  ╚═╝╚══════╝v.0.5 by busjr
 --------------------------------------
 [1] - Encrypt
 [2] - Decipher
 [3] - Back
-[4] - Exit""")
+[4] - Enable context menu
+[5] - Disable context menu
+[6] - Exit"""
+        )
         input_user = int(input(">"))
         if input_user == 1:
             Encrypt()
         elif input_user == 2:
             Decrypt()
         elif input_user == 4:
+            menu()
+        elif input_user == 5:
+            menu_delete()
+        elif input_user == 6:
             exit()
         else:
-            system('cls')
+            system("cls")
             Choice()
     except ValueError:
-        system('cls')
+        system("cls")
+        Choice()
+
+
+def menu():
+    try:
+        script_path = sys.argv[0]
+        base_path = r"HKEY_CLASSES_ROOT\Directory\Background\shell"
+        key_name = "ERE"
+        command_key_path = f"{base_path}\\{key_name}\\command"
+        subprocess.run(
+            [
+                "reg",
+                "add",
+                f"{base_path}\\{key_name}",
+                "/ve",
+                "/t",
+                "REG_SZ",
+                "/d",
+                "Open in ERE",
+                "/f",
+            ],
+            check=True,
+        )
+        subprocess.run(
+            [
+                "reg",
+                "add",
+                command_key_path,
+                "/ve",
+                "/t",
+                "REG_SZ",
+                "/d",
+                script_path,
+                "/f",
+            ],
+            check=True,
+        )
+        system("cls")
+        print("Done")
+        Choice()
+    except Exception:
+        system("cls")
+        print(
+            "\033[31m"
+            + (
+                'If you want to enable the context menu, click "Yes" '
+                "(When moving .exe file, choice here again)." + "\033[0m"
+            )
+        )
+        Choice()
+
+
+def menu_delete():
+    try:
+        path = r"HKEY_CLASSES_ROOT\Directory\Background\shell\ERE"
+        command = ["reg", "delete", path, "/f"]
+        subprocess.run(command, check=True)
+        system("cls")
+        print("Done")
+        Choice()
+    except Exception:
+        system("cls")
+        print(
+            "\033[31m"
+            + (
+                'If you want to enable the context menu, click "Yes" '
+                "(When moving .exe file, choice here again)." + "\033[0m"
+            )
+        )
         Choice()
 
 
 def Encrypt():
     try:
-        path = input('Path file for encrypt: ')
-        if path != '3':
+        path = input("Path file for encrypt: ")
+        if path != "3":
             path.replace("/", "\\")
-            password = getpass("password: ",  stream="*")
+            password = getpass("password: ", stream="*")
             encryptFile(path, path + ".aes", password)
         else:
-            system('cls')
+            system("cls")
             Choice()
     except Exception:
-        system('cls')
-        print("\033[31m" + ('ERROR: File not found or file extension not '
-                            'supported. (Please check the full path to the '
-                            'file or its .aes extension and open the program '
-                            'with administrator rights.' + "\033[0m"))
+        system("cls")
+        print(
+            "\033[31m"
+            + (
+                "ERROR: File not found or file extension not "
+                "supported. (Please check the full path to the "
+                "file or its .aes extension and open the program "
+                "with administrator rights." + "\033[0m"
+            )
+        )
         Choice()
 
 
 def Decrypt():
     try:
-        path = input('Path file for decrypt: ')
-        if path != '3':
+        path = input("Path file for decrypt: ")
+        if path != "3":
             path.replace("/", "\\")
             password = getpass("password: ", stream="*")
             decryptFile(path, path + " dataout.txt", password)
         else:
-            system('cls')
+            system("cls")
             Choice()
     except Exception:
-        system('cls')
-        print("\033[31m" + ('ERROR: File not found or file extension not '
-                            'supported. (Please check the full path to the '
-                            'file or its .aes extension and open the program '
-                            'with administrator rights.' + "\033[0m"))
+        system("cls")
+        print(
+            "\033[31m"
+            + (
+                "ERROR: File not found or file extension not "
+                "supported. (Please check the full path to the "
+                "file or its .aes extension and open the program "
+                "with administrator rights." + "\033[0m"
+            )
+        )
         Choice()
 
 
