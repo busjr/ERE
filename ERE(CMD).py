fb5ba559da85a9aee139f3584a8d769d4ce49464
busjr <busjr1337@mail.ru>
2025-02-01 19:22:10 +0300

    fix: bugs

--- ERE(CMD).py
+++ ERE(CMD).py
@@ -1,17 +1,26 @@
 import sys
 from os import system, name
+from pathlib import Path
 from getpass import getpass
 from pyAesCrypt import encryptFile, decryptFile
 
 
-def ClearCMD():
-    if name == 'nt':
+def clear_cmd():
+    if name == 'nt': # Windows
         system("cls")
-    else:
+    else: # macOS, Linux
         system("clear")
 
 
-def Main():
+def clear_path(path):
+    if name == 'nt': # Windows
+        path = path.replace("/", "\\")
+    else: # macOS, Linux
+        path = path.replace("'", "")
+    return path
+
+
+def main():
     while True:
         try:
             print(
@@ -30,64 +39,64 @@ def Main():
             )
             input_user = int(input(">"))
             if input_user == 1:
-                Encrypt()
+                encrypt()
             elif input_user == 2:
-                Decrypt()
+                decrypt()
             elif input_user == 3:
-                ClearCMD()
+                clear_cmd()
             elif input_user == 4:
                 sys.exit()
             else:
-                ClearCMD()
+                clear_cmd()
                 print("\033[31mERROR: Invalid input.\033[0m")
         except ValueError:
-            ClearCMD()
+            clear_cmd()
             print("\033[31mERROR: Invalid input.\033[0m")
 
 
-def Encrypt():
+def encrypt():
     try:
         path = str(input("Path file for encrypt: "))
         if path != "3":
-            path.replace("/", "\\")
+            path = clear_path(path)
             password = getpass("password: ")
             encryptFile(path, path + ".aes", password)
-            ClearCMD()
+            clear_cmd()
             print("\033[32m" + path + "\033[0m")
         else:
-            ClearCMD()
+            clear_cmd()
     except FileNotFoundError:
-        ClearCMD()
+        clear_cmd()
         print("\033[31mERROR: File not found.\033[0m")
     except ValueError:
-        ClearCMD()
+        clear_cmd()
         print("\033[31mERROR: Invalid input.\033[0m")
     except Exception:
-        ClearCMD()
-        print("\033[31mERROR: Invalid input.\033[0m")
+        clear_cmd()
+        print("\033[31mERROR\033[0m")
 
 
-def Decrypt():
+def decrypt():
     try:
         path = str(input("Path file for decrypt: "))
         if path != "3":
-            path.replace("/", "\\")
-            password = getpass("password: ", stream="*")
+            path = clear_path(path)
+            password = getpass("password: ")
             decryptFile(path, path + " dataout.txt", password)
-            ClearCMD()
+            clear_cmd()
             print("\033[32m" + path + "\033[0m")
         else:
-            ClearCMD()
+            clear_cmd()
     except FileNotFoundError:
-        ClearCMD()
+        clear_cmd()
         print("\033[31mERROR: File not found.\033[0m")
     except ValueError:
-        ClearCMD()
+        clear_cmd()
         print("\033[31mERROR: Invalid input.\033[0m")
     except Exception:
-        ClearCMD()
-        print("\033[31mERROR: Invalid input.\033[0m")
+        clear_cmd()
+        print("\033[31mERROR\033[0m")
 
 
 if __name__ == "__main__":
-    Main()
+    main()
